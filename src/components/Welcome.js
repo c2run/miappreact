318f875dd0b07ff2f288f92d2a819f67b3d9323e
c2run <jose.cerda66@gmail.com>
2023-03-21 14:29:39 -0300

    Hook useEffect semaforo contador

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,8 +3,17 @@ import { useState } from "react";
 export default function Welcome(props){
  //console.log(props)
 
- const [counter, setCounter] = useState(0);
-const { message, name } = props;
+    //hooks sirven para guardar estados
+    const [counter, setCounter] = useState(0);
+    const { message, name } = props;
+
+    //
+    const [semaforo, setSemaforo] = useState(false);
+    const contar = () => {
+        //console.log("Entrando en la función de contar");
+        setCounter(counter + 1);
+        setSemaforo(!semaforo)
+    };
 
 return (
 <div>
@@ -12,9 +21,7 @@ return (
     <p>{message}</p>
     <h2>Contador de React con Hooks</h2>
     <h4>El número del contador es {counter}</h4>
-    <button type="submit" onClick={() => setCounter(counter+1)}>
-        Sumar contador
-    </button>
+    <button type="submit" onClick={contar}>Sumar</button>
 </div>
 );
 
