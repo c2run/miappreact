513d55985795993532c86f534d4d080f8b238ca9
c2run <jose.cerda66@gmail.com>
2023-03-22 11:00:27 -0300

    pasar slug perfil react router dom dinámico

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Welcome from './components/Welcome';
 import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import  Contact from "./pages/Contact"; 
 import Aboutme from "./pages/AboutMe";
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -24,11 +25,15 @@ function App() {
             <li>
               <Link to="/about-me">About me</Link>
             </li>
+            <li>
+              <Link to="/profile">Perfil</Link>
+            </li>
           </ul>
         </div>
          <Routes>
           <Route path="/contact" element={<Contact />} />
           <Route path="/about-Me" element={<Aboutme />} />
+          <Route path="/profile/:name" element={<Profile />}  />
         </Routes>
        </Router>
       </header>
